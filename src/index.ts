7f422bb08a139d11da1a3c07dd94c9a616c5421f
Jose Mesa <jose.mesa.solorzano@gmail.com>
2018-07-07 12:07:45 +0200

    Initial configuration for database and server

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata'
+import {createKoaServer} from 'routing-controllers'
+import setupDb from './dbs'
+
+
+
+const app = createKoaServer({
+  cors: true ,
+  controllers: [  ]
+})
+
+setupDb()
+  .then(_ =>
+    app.listen(4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
